409440c8f4f2e50782c2ce371aacc0cafb7af2cf
SIM <simon.wirtz@n-design.de>
2017-09-21 14:55:12 +0200

    added index.html

--- src/main/kotlin/de/swirtz/springwebflux/SpringwebfluxKotlinDemoApplication.kt
+++ src/main/kotlin/de/swirtz/springwebflux/SpringwebfluxKotlinDemoApplication.kt
@@ -2,6 +2,8 @@ package de.swirtz.springwebflux
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Spring WebFlux is the new reactive web framework introduced in Spring Framework 5.0.
@@ -15,3 +17,7 @@ fun main(args: Array<String>) {
     SpringApplication.run(SpringwebfluxKotlinDemoApplication::class.java, *args)
 }
 
+
+@RequestMapping("/")
+@RestController
+class IndexController
